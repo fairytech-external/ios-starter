5795f4eba0666507d2f1592f39e3ad5054f2db86
davidjkjk <david@fairytech.ai>
2024-01-04 21:32:08 +0900

    Add example app

---
+++ FairyExample/PacketTunnel/PacketTunnelProvider.swift
@@ -0,0 +1,40 @@
+/*
+ Fairy Technologies CONFIDENTIAL
+ __________________
+  
+ Copyright (C) Fairy Technologies, Inc - All Rights Reserved
+ 
+ NOTICE:  All information contained herein is, and remains
+ the property of Fairy Technologies Incorporated and its suppliers,
+ if any.  The intellectual and technical concepts contained
+ herein are proprietary to Fairy Technologies Incorporated
+ and its suppliers and may be covered by U.S. and Foreign Patents,
+ patents in process, and are protected by trade secret or copyright law.
+ Dissemination of this information, or reproduction or modification of this material
+ is strictly forbidden unless prior written permission is obtained
+ from Fairy Technologies Incorporated.
+*/
+
+import Moment
+import UIKit
+
+class PacketTunnelProvider: Moment.PacketTunnelProvider {
+    
+    override func handleRecognitionResult(recognizedBusinessID: String) async {
+        let notificationContent = UNMutableNotificationContent()
+
+        notificationContent.title = "Fairy Example"
+        notificationContent.body = "\(recognizedBusinessID) 인식"
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.5, repeats: false)
+        let request = UNNotificationRequest(identifier: "testNotification",
+                                            content: notificationContent,
+                                            trigger: trigger)
+
+        do {
+            try await UNUserNotificationCenter.current().add(request)
+        } catch {
+            print("Notification Error: ", error)
+        }
+    }
+}
